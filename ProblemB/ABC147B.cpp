e5565073702ef6cdddb622c4c1d287016e7a7874
mumumumu <prestofuture00@gmail.com>
2020-02-03 16:09:19 +0100

    ABC147-B
    python and cpp

---
+++ ProblemB/ABC147B.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+//macros
+using namespace std;
+typedef long long ll;
+typedef pair<int, int > Pii;
+typedef pair<ll, ll> Pll;
+typedef vector<int> Vi;
+typedef vector<ll> Vll;
+#define pb push_back
+#define rs resize 
+#define rep(i, a, b) for(int i=(a); i<(b); i++)
+#define repll(i, a, b) for(ll i=(a); i<(b); i++)
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define COUT(a) cout << a << endl
+#define ALL(a) (a).begin(),(a).end()
+const long long int mod = 1e9 + 7;
+
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false); 
+    string S;
+    cin >> S;
+    int cnt = 0;
+
+    rep(i,0,S.size()/2){
+        if(S[i]==S[S.size()-i-1]) continue;
+        cnt++;
+    }    
+    COUT(cnt);
+    
+}
